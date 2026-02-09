6d9e957d784bc3adf44ffd00e8d1e768bfdaacb5
dralovets-dot <dralovets@gmail.com>
2026-02-09 10:40:43 +0300

    Fix: init SPI before TFT, manual pin setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <SPI.h>
 
-// ВАЖНО: Сначала подключаем настройки!
+// Настройки ДО подключения TFT_eSPI
+#define USER_SETUP_LOADED
 #include "User_Setup.h"
 #include "TFT_eSPI.h"
 
-// Официальные пины
 #define BOARD_LORA_CS   12
 #define BOARD_SD_CS     13
 
@@ -14,35 +15,44 @@ TFT_eSPI tft;
 void setup() {
     WiFi.mode(WIFI_OFF);
     Serial.begin(115200);
-    delay(500);
+    delay(1000);
     
-    // CS пины в HIGH
-    pinMode(TFT_CS, OUTPUT);
-    digitalWrite(TFT_CS, HIGH);
-    pinMode(BOARD_SD_CS, OUTPUT);
-    digitalWrite(BOARD_SD_CS, HIGH);
-    pinMode(BOARD_LORA_CS, OUTPUT);
-    digitalWrite(BOARD_LORA_CS, HIGH);
+    Serial.println("Starting...");
     
-    // Питание
+    // 1. Включаем питание периферии (ОБЯЗАТЕЛЬНО!)
     pinMode(46, OUTPUT);
     digitalWrite(46, HIGH);
-    delay(200);
+    delay(500);
+    
+    // 2. Подтягиваем ВСЕ CS к HIGH (важно!)
+    pinMode(BOARD_LORA_CS, OUTPUT);
+    digitalWrite(BOARD_LORA_CS, HIGH);
+    pinMode(BOARD_SD_CS, OUTPUT);
+    digitalWrite(BOARD_SD_CS, HIGH);
+    
+    // 3. Инициализируем SPI вручную ДО tft.begin()
+    Serial.println("Init SPI...");
+    SPI.begin(TFT_SCLK, -1, TFT_MOSI, TFT_CS); // SCK, MISO (не используется), MOSI, SS
+    
+    // 4. Теперь инициализируем TFT
+    Serial.println("Init TFT...");
     
     // Подсветка
     pinMode(TFT_BL, OUTPUT);
     digitalWrite(TFT_BL, HIGH);
     
-    Serial.println("Init TFT...");
-    tft.begin();
+    // Создаём TFT с явными размерами
+    tft.init();
     tft.setRotation(1);
     tft.fillScreen(TFT_BLACK);
     
     Serial.println("TFT OK!");
+    
+    // Тест
     tft.setTextColor(TFT_GREEN, TFT_BLACK);
     tft.setTextSize(2);
-    tft.setCursor(20, 80);
-    tft.print("ESP-HACK OK!");
+    tft.setCursor(20, 100);
+    tft.print("WORKING!");
 }
 
 void loop() {
